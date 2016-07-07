a0c465f62df2d71813cc81a05be8157ea7e8d3e6
Michael Pitidis <mpitid@gmail.com>
2016-07-07 21:19:41 +0100

    Implement exercise 1.3

--- cli.hs
+++ cli.hs
@@ -5,8 +5,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let a1 = read $ args !! 0
-  let a2 = read $ args !! 1
-  putStrLn $ "Hello, " ++ show (a1 + a2)
+  name <- getLine
+  putStrLn $ "Hello, " ++ name
 
