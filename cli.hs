310ade530e01e18dd8418402729fa13e0c42fafb
Michael Pitidis <mpitid@gmail.com>
2016-07-07 22:29:51 +0100

    Simple whitespace/symbol parser

--- cli.hs
+++ cli.hs
@@ -1,10 +1,22 @@
 
 module Main where
 
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value " ++ show val
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 main :: IO ()
 main = do
-  name <- getLine
-  putStrLn $ "Hello, " ++ name
+  expr : _ <- getArgs
+  putStrLn $ readExpr expr
 
