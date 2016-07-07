b5503c93ee9bda0fc864ebb1393c66cc81e880bc
Michael Pitidis <mpitid@gmail.com>
2016-07-07 22:53:32 +0100

    exercise 2.1.1

--- lisper.hs
+++ lisper.hs
@@ -32,7 +32,10 @@ parseAtom = do
     _    -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+-- parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = do
+  digits <- many1 digit
+  return $ Number $ read digits
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
